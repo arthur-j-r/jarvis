0647466b9dc95a027660b08b0da9bb4634b61a06
Arthur José Rocha <arthurjoserocha802@gmail.com>
2026-08-26 16:19:10 -0300

    Arquivos Ocultados

--- teste.py
+++
@@ -1,7 +0,0 @@
-import sounddevice as sd
-fs = 44100  # Sample rate
-
-duration = 10.5  # seconds
-myrecording = sd.rec(int(duration * fs), samplerate=fs, channels=2)
-sd.wait()  # Wait until recording is finished
-sd.play(myrecording, fs)
